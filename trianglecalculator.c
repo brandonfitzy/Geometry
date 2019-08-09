920ecd96cbf10102c09cb7e6b39b6f3a983dc6a4
Brandon Fitzpatrick <49811439+brandonfitzy@users.noreply.github.com>
2019-08-09 10:10:50 -0400

    Update trianglecalculator.c

--- trianglecalculator.c
+++ trianglecalculator.c
@@ -1,6 +1,7 @@
 /*Triangle Calculator*/
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 struct triangle{
@@ -22,32 +23,36 @@ int main() {
 
     one.x1=0;
     one.y1=0;
-    one.x2=3;
-    one.y2=0;
+    one.x2=0;
+    one.y2=4;
     one.x3=3;
-    one.y3=4;
+    one.y3=0;
     
-    double area=areacalc();
+    areacalc();
 
     
 	return 0;
 }
 
 double areacalc(){
-    double base = sqrt(((one.x1-one.x2)*(one.x1-one.x2))+((one.y1-one.y2)*(one.y1-one.y2)));
-    printf("%lf \n",base);
+    double base = abs(sqrt(((one.x1-one.x2)*(one.x1-one.x2))+((one.y1-one.y2)*(one.y1-one.y2))));
     double m1 = ((one.y1-one.y2)/(one.x1-one.x2));
-    printf("%lf \n",m1);
     double b1 = one.y1-m1 * one.x1;
-    printf("%lf \n",b1);
+	double height;
 /* issue here for when the slope is 0 or the line is vertical  */
-    double m2 = pow(m1,1);
-    printf("%lf \n",m2);
-    double b2 = one.y3-m2 * one.x3;
-    double ix = (b1-b2)/(m1-m2);
-    double iy = m1 * ix+b1;
-    double height = sqrt(((one.x3-ix) * (one.x3-ix))+((one.y3-iy) * (one.y3-iy)));
-    double area = 0.5 * base * height;
-    printf("%lf",area);
+	if (m1==0){
+		height= abs(one.y3-one.y1);
+	} else if (one.x1==one.x2){
+		height= abs(one.x3-one.x1);
+	} else {
+		double m2 = pow(m1,-1);
+		double b2 = one.y3-m2 * one.x3;
+		double ix = (b1-b2)/(m1-m2);
+		double iy = m1 * ix+b1;
+		height = abs(sqrt(((one.x3-ix) * (one.x3-ix))+((one.y3-iy) * (one.y3-iy))));
+	}
+	double area = 0.5 * base * height;
+	printf("%lf",area);
+	
     return area;
 }
