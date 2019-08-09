3a83045084224b09a9c804a0418985e999bbd31a
Brandon Fitzpatrick <49811439+brandonfitzy@users.noreply.github.com>
2019-08-09 10:36:05 -0400

    Update trianglecalculator.c

--- trianglecalculator.c
+++ trianglecalculator.c
@@ -16,7 +16,7 @@ struct triangle{
 struct triangle one;
 
 double areacalc();
-
+void translate(int x, int y);
 
 
 int main() {
@@ -29,9 +29,31 @@ int main() {
     one.y3=0;
     
     areacalc();
-
+	translate(2,2);
     
 	return 0;
+}
+
+void yreflect(int x){
+	one.x1=abs(2*(one.x1-x));
+	one.x2=abs(2*(one.x2-x));
+	one.x3=abs(2*(one.x3-x));
+}
+
+void xreflect(int y){
+	one.y1=abs(2*(one.y1-y));
+	one.y2=abs(2*(one.y2-y));
+	one.y3=abs(2*(one.y3-y));
+}
+
+void translate(int x, int y){
+	one.x1+=x;
+    one.y1+=y;
+    one.x2+=x;
+    one.y2+=y;
+    one.x3+=x;
+    one.y3+=y;
+	printf("The new triangle coordinates are:\n	p1=(%d,%d) p2=(%d,%d) p3=(%d,%d)",one.x1,one.y1,one.x2,one.y2,one.x3,one.y3);
 }
 
 double areacalc(){
